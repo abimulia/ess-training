0b9e651383b21efa27d2772570208b5ff4f9a94c
abimulia <abimulia@gmail.com>
2024-10-22 21:59:45 +0700

    add implementation for apply now

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -119,4 +119,8 @@ export class HousingService {
       (housingLocation) => housingLocation.id === id
     );
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log('Submitted: ' + firstName + ' ' + lastName + ' ' + email);
+  }
 }
