d15dd99c5dfac8386f8043ce8a8c31622816ab93
abimulia <abimulia@gmail.com>
2024-10-22 21:37:42 +0700

    add implementation to show housing details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,20 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
-  constructor(){
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
-
 }
